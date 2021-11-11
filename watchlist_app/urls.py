b16a930f454994debd6e04a997526bba7322cbb9
Hosne Mubarak <h.m.tasmir@gmail.com>
2021-11-12 00:15:33 +0600

    fix

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -10,15 +10,15 @@ urlpatterns = [
     # path('list', movie_list, name='movie-list'),
     # path('movie/<int:pk>', movie_details, name='movie_detailss'),
 
-    path('list', MovieListListAV.as_view(), name='movie-list'),
-    path('<int:pk>', MovieListDetailsAV.as_view(), name='movie_details'),
+    path('list/', MovieListListAV.as_view(), name='movie-list'),
+    path('<int:pk>/', MovieListDetailsAV.as_view(), name='movie_details'),
     path('', include(router.urls)),
     # path('streem', StreamPlatformListAV.as_view(), name='platform_list'),
     # path('streem/<int:pk>', StreamPlatformDetailsAV.as_view(), name='streamplatform-detail'),
 
     # path('review', ReviewList.as_view(), name='review-list'),
     # path('review/<int:pk>', ReviewDetail.as_view(), name='review-details')
-    path('<int:pk>/review-create', ReviewCreate.as_view(), name='review-create'),
-    path('<int:pk>/review', ReviewList.as_view(), name='review-list'),
-    path('review/<int:pk>', ReviewDetail.as_view(), name='review-details')
+    path('<int:pk>/review-create/', ReviewCreate.as_view(), name='review-create'),
+    path('<int:pk>/review/', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>/', ReviewDetail.as_view(), name='review-details')
 ] 
